d3c26cd5d7434d9693d9bcd49d610c6f8b7c74ec
Ryano2000 <League@AdministorsiMac.attlocal.net>
2018-06-20 11:50:03 -0700

    Day 3

---
+++ src/section3/OwnAdventure.java
@@ -0,0 +1,9 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class OwnAdventure {
+public static void main(String[] args) {
+JOptionPane.showOptionDialog(null, "hello, title, optionType, messageType, icon, options, initialValue)
+}
+}
--- src/section3/StephenHawking.java
+++ src/section3/StephenHawking.java
@@ -5,15 +5,17 @@ import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
 public class StephenHawking {
+	public static void main(String[] args) {
+		
 	
 	// 1. make a main method and put steps 2, 3 & 4 inside it
 	
 	// 4. Use a for loop to repeat steps #2 and #3, a lot of times
-	
+	for(int i=0;i<10;i++) {
 	// 2. ask the user for a sentence
-	
+	String word = JOptionPane.showInputDialog("Type a Sentence");
 	// 3. call the speak method below and send it the sentence
-
+speak(word);
 	
 
 	
@@ -21,8 +23,8 @@ public class StephenHawking {
 	
 	
 	
-	
-	
+	}	
+	}	
 	/* Don’t change this…. */
 	static void speak(String words) {
 		try {
